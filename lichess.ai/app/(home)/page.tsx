7524ec49303afcff0e2582e6d6a5fe5b478ceb13
Josiah Plett <jlplett@uwaterloo.ca>
2024-09-27 19:46:50 +0200

    update: Home page links

--- lichess.ai/app/(home)/page.tsx
+++ lichess.ai/app/(home)/page.tsx
@@ -4,7 +4,16 @@ export default async function Home() {
   return (
     <main className="flex flex-col gap-2 my-auto items-center">
       <h1>Lichess.ai</h1>
-      <p>A little data analysis website.</p>
+      <p>
+        A{" "}
+        <a
+          className="no-underline text-blue-900 hover:to-blue-700"
+          href="https://database.lichess.org/#standard_games"
+        >
+          lichess database
+        </a>{" "}
+        data analysis website.
+      </p>
       <hr className="h-2" />
       <Button asChild className="w-64 my-2">
         <a
@@ -12,7 +21,7 @@ export default async function Home() {
           href="https://kurser.dtu.dk/course/02450"
           rel="noopener noreferrer"
         >
-          Data
+          Course Link
         </a>
       </Button>
     </main>
